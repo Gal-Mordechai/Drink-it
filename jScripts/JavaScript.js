99a901e80a8e230f272eb92abd753366224eae32
Gal-Mordechai <86434949+Gal-Mordechai@users.noreply.github.com>
2021-06-26 22:52:29 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,42 @@
+﻿// When the user scrolls down 20px from the top of the document,we will show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scroll-btn").style.display = "block";
+    }
+    else {
+        document.getElementById("scroll-btn").style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+function tab_nav() {
+    var navA = document.querySelectorAll("ul li>a");
+    var navInner = document.querySelectorAll("li ul li>a");
+
+    for (var i = 0; i < navA.length; i++) {
+        navA[i].addEventListener("focus", function (event) {
+            var innerUl = event.target.parentElement.querySelector("ul")
+            if (innerUl) innerUl.style.display = "block";
+        }, true);
+    }
+
+    for (var i = 0, len = navInner.length; i < len; i++) {
+        if (navInner[i].parentElement.nextElementSibling === null) {
+            navInner[i].addEventListener("blur", function (event) {
+                event.target.parentElement.parentElement.style.display = "none"
+            }, true);
+        }
+    }
+}
+
+$(function () {
+    $('.scroll-down').click(function () {
+        $('html, body').animate({ scrollTop: $('section.ok').offset().top }, 'slow');
+        return false;
+    });
+});
---
+++ jScripts/JavaScript2.js
@@ -0,0 +1,9 @@
+﻿var is18 = confirm("האם אתה מעל גיל 18?");
+console.log(is18);
+
+if (is18 == false) {
+    window.location.href = "https://google.com";
+}
+else {
+    window.href = "homepage.html";
+}
